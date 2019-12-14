7035dd4ac5d3ccd0a7266b55f75d61b465e3d80d
Rasmus Schultz <rasmus@mindplay.dk>
2019-12-14 12:29:18 +0100

    use `mocha` and `chai` instead of `jest`; ported one test to get this rolling

--- test/h.test.jsx
+++ test/h.test.jsx
@@ -1,11 +1,13 @@
 /** @jsx h */
 
 import { h } from "../src/h"
+import { it } from "mocha"
+import { expect } from "chai"
 
-test('create JSX node', () => {
+it('creates JSX node', () => {
   const div = <div/>
 
-  expect(div).toStrictEqual({
+  expect(div).to.deep.equal({
     type: "div",
     key: null,
     ref: null,
@@ -13,10 +15,10 @@ test('create JSX node', () => {
   })
 })
 
-test('create JSX node with key and props', () => {
+it('creates JSX node with key and props', () => {
   const div = <input key="foo" name="foo" value="bar"/>
 
-  expect(div).toStrictEqual({
+  expect(div).to.deep.equal({
     type: "input",
     key: "foo",
     ref: null,
@@ -27,7 +29,7 @@ test('create JSX node with key and props', () => {
   })
 })
 
-test('create JSX node with children', () => {
+it('creates JSX node with children', () => {
   const divs = (
     <div key="a">
       <div key="b">
@@ -36,7 +38,7 @@ test('create JSX node with children', () => {
     </div>
   )
 
-  expect(divs).toStrictEqual({
+  expect(divs).to.deep.equal({
     type: "div",
     key: "a",
     ref: null,
@@ -58,10 +60,10 @@ test('create JSX node with children', () => {
   })
 })
 
-test('ignore `true`, `false`, `null` and `undefined` JSX literals', () => {
+it('ignores `true`, `false`, `null` and `undefined` JSX literals', () => {
   const div = <div>{true}{false}{null}{undefined}</div>
 
-  expect(div).toStrictEqual({
+  expect(div).to.deep.equal({
     type: "div",
     key: null,
     ref: null,
@@ -69,10 +71,10 @@ test('ignore `true`, `false`, `null` and `undefined` JSX literals', () => {
   })
 })
 
-test('emit JSX string/number literals', () => {
+it('emits JSX string/number literals', () => {
   const div = <div>{"hello"}{""}{123}</div>
 
-  expect(div).toStrictEqual({
+  expect(div).to.deep.equal({
     type: "div",
     key: null,
     ref: null,
@@ -86,12 +88,12 @@ test('emit JSX string/number literals', () => {
   })
 })
 
-test('emit JSX component nodes', () => {
+it('emits JSX component nodes', () => {
   const Component = ({ value }) => <input value={value}/>
 
   const div = <Component value={"foo"}>bar</Component>
 
-  expect(div).toStrictEqual({
+  expect(div).to.deep.equal({
     type: Component,
     key: null,
     ref: null,
